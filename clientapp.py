6c0d7d7ab1b0de9a23bc42a582949c23f65182e2
cyberatlas <10457502+cyberatlas@users.noreply.github.com>
2019-12-03 22:09:32 -0600

    Added the creation of sentences.txt and refactored a little

--- clientapp.py
+++ clientapp.py
@@ -6,59 +6,75 @@ import argparse
 # NOTE: must have WaveRNN and its deps, must have SOX installed
 # TODO clean up by letting it take args
 
-# Parse the args for file location
-parser = argparse.ArgumentParser(description = "Reads in a pdf or txt file, outputs an audio file of it being read")
-parser.add_argument('file', help = "The file you would like to input")
-args = parser.parse_args()
-file = args.file
-
-# TODO might need to make this more robust
-# Check the extension of the file and save the texxt
-if file[-3:] == 'pdf':
+# Takes input stream and writes one line at a time to sentences.txt
+def make_sentences(textin):
     try:
-        with open(file, "rb") as text:
-            pdf = pdftotext.PDF(text)
-
-        print("Number of pages: ", len(pdf))
-        print("\n\n".join(pdf))
-    except IOError:
-        print("Could not open the file!")
-
-    print("WIP")
-elif file[-3:] == 'txt':
-    f = open(file, 'r')
-    text = f.read()
-    print(text)
-    f.close()
-
-    print("wip")
-else:
-    print("Input a txt or pdf file")
-    exit(1)
+        # Opens the file to write, overrites if exists and creates if it doesn't
+        # By using with statements files are automatically closed for us
+        with open("sent.txt", "w+") as sent:
+            for line in textin:
+                sent.write(line)
+    except IOError as e:
+        print("Could not open/create sentences.txt")
+        print(e)
+        exit(1)
 
 
-# Delete sentences.txt if it already exists
-if os.path.exists("sentences.txt"):
-    os.remove("sentences.txt")
+def main():
+    # Parse the args for file location
+    parser = argparse.ArgumentParser(description = "Reads in a pdf or txt file, outputs an audio file of it being read")
+    parser.add_argument('file', help = "The file you would like to input")
+    args = parser.parse_args()
+    # Saving the file location
+    file = args.file
+    
+    # TODO might need to make this more robust
+    # Check the extension of the file and save the texxt
+    if file[-3:] == 'pdf':
+        try:
+            with open(file, "rb") as file_in:
+                text = pdftotext.PDF(file_in)
+                
+            print("Number of pages: ", len(text))
+           # print("\n\n".join(pdf))
+        except IOError as e:
+            print("Could not open the file!")
+            print(e)
+            exit(1)
+    elif file[-3:] == 'txt':
+        #f = open(file, 'r')
+        with open(file, 'r') as file_in:
+            text = file_in.read()
+    else:
+        print("Input a txt or pdf file")
+        exit(1)
 
-# Delete the quick_start dir if exists, does not do anything if it does not
-os.system("rm -rf quick_start/")
+    make_sentences(text)
 
-# TODO Save our file 1 line at a time to sentences.txt
+    # Delete sentences.txt if it already exists
+    if os.path.exists("sentences.txt"):
+        os.remove("sentences.txt")
 
-# Run wavrnn
-os.system("python Wavernn/quick_start.py")
-
-out_file = file + ".wav"
-
-# Concatenate the wav files and save as the output file
-concat_cmd = "sox WaveRNN/quick_start/*wav " + out_file
-os.system(concat_cmd)
-
-# TODO output the file to the user
-os.system("ls")
-print("Your generated audio is ", out_file)
+    # Delete the quick_start dir if exists, does not do anything if it does not
+    os.system("rm -rf quick_start/")
+    
+    # Run wavrnn
+    os.system("python Wavernn/quick_start.py")
+    
+    out_file = file + ".wav"
+    
+    # Concatenate the wav files and save as the output file
+    concat_cmd = "sox WaveRNN/quick_start/*wav '" + out_file + "'"
+    os.system(concat_cmd)
+    
+    # TODO output the file to the user
+    os.system("ls")
+    print("Your generated audio is ", out_file)
 
 
 # TODO could add ability to save as mp3
 # TODO add error checking around the os.system stuff
+# TODO Add ability to do page range
+# TODO add ability to preview page first
+
+main()
